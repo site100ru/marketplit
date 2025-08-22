1be5376baa35255fb034047d8814834c64b817a6
Slava Sidorov <sidorov-vv3@mail.ru>
2025-08-22 14:13:20 +0500

    вернул почту

--- mails/message-mail.php
+++ mails/message-mail.php
@@ -5,8 +5,8 @@
 	$email = $_POST['email'];
 	$mes = $_POST['mes'];
 
-	// mail("1752800@mail.ru, vasilyev-r@mail.ru", "Сообщение с сайта interflat-spb.ru", "
-	mail("sidorov-vv3@mail.ru, vasilyev-r@mail.ru", "Сообщение с сайта interflat-spb.ru", "
+	// mail("sidorov-vv3@mail.ru, vasilyev-r@mail.ru", "Сообщение с сайта interflat-spb.ru", "
+	mail("1752800@mail.ru, vasilyev-r@mail.ru", "Сообщение с сайта interflat-spb.ru", "
 		Клиент: ".$name."\n
 		Email: ".$email."\n
 		Сообщение: ".$mes."\n
